45449c3e1fddd5df6941f88a5bf456e4674e2ad7
divyasarvan <ndivyaaravind@gmail.com>
2022-11-06 17:26:20 +0530

    welcome

---
+++ src/test/java/org/hcl/Gitnewproject/develop.java
@@ -0,0 +1,24 @@
+package org.hcl.Gitnewproject;
+
+public class develop {
+	
+		public void test1() {
+		System.out.println("test1");
+		}
+		
+		public void test2() {
+			System.out.println("test2");
+		}
+		
+		public void test3() {
+			System.out.println("test3");
+		}
+
+		public void test4() {
+			System.out.println("test4");
+		}
+	}
+
+
+
+
