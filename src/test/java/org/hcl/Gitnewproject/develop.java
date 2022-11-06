7e93b121b20d561e2d88d32d09db36d2c322bde4
divyasarvan <ndivyaaravind@gmail.com>
2022-11-06 20:05:15 +0530

    modified

--- src/test/java/org/hcl/Gitnewproject/develop.java
+++ src/test/java/org/hcl/Gitnewproject/develop.java
@@ -17,20 +17,7 @@ public class develop {
 		public void test4() {
 			System.out.println("test4");
 		}
-		public void test5() {
-			System.out.println("test5");
-			}
-			
-			public void test6() {
-				System.out.println("test6");
-			}
-			
-			public void test7() {
-				System.out.println("test7");
-			}
-
-			public void test8() {
-				System.out.println("test8"); }
+		
 				public void test9() {
 					System.out.println("test9");
 				}
